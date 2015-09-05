26578bd374fab0503a491b7f953c2f76ba25cbec
Kashav Madan <kshvmdn@gmail.com>
2015-09-05 01:09:29 -0400

    Login+register front-end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var title = "PennyPool";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: title });
+});
+
+router.get('/register', function(req,res,next) {
+	res.render('reg_log', {title: title, type: 'reg'});
+});
+
+router.get('/login', function(req,res,next) {
+	res.render('reg_log', {title: title, type: 'login'});
 });
 
 module.exports = router;
